d5ae94c589e1ec206ca3ac379a8d9fb376ea487b
1800209 <eam152@student.aru.ac.uk>
2021-02-18 10:06:44 +0000

    Renamed typo of TEMP_DIR

--- app/main.py
+++ app/main.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 flite = Command("/app/flite").bake("-voice", "rms")
 ffmpeg = Command("ffmpeg").bake("-y")
 SUFFIX = ".ogg"
-TEMPR_DIR = "/tmp"
+TEMP_DIR = "/tmp"
 
 def synthesise_speech(text: str, tempfile):
-  with NamedTemporaryFile(suffix=SUFFIX, dir=TEMPR_DIR) as temp_intermediate:
+  with NamedTemporaryFile(suffix=SUFFIX, dir=TEMP_DIR) as temp_intermediate:
     tempfile_intermediate = temp_intermediate.name
     flite(text, tempfile_intermediate)
     ffmpeg(tempfile, i=tempfile_intermediate)
@@ -17,7 +17,7 @@ def synthesise_speech(text: str, tempfile):
 @app.route('/', methods=['POST'])
 def get_speech():
   text = request.json['text']
-  with NamedTemporaryFile(suffix=SUFFIX, dir=TEMPR_DIR) as temp:
+  with NamedTemporaryFile(suffix=SUFFIX, dir=TEMP_DIR) as temp:
     tempfile = temp.name
     synthesise_speech(text, tempfile)
     return send_file(tempfile)
